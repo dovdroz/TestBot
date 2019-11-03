9b9ee1d91f9b10e11ae58d535ee34552fbb31a8e
dovdroz <48339608+dovdroz@users.noreply.github.com>
2019-11-02 20:34:43 -0400

    Update app.py

--- app.py
+++ app.py
@@ -29,6 +29,9 @@ def msg_received_from_group():
 
   elif data['text'].lower() == "!testpic":
     send_msg_pic("Hello World!","https://i.groupme.com/1024x1024.jpeg.d733d6de5c36462f8d1cb67e3191b618")
+
+  elif data['text'].lower() == "what's the weather today?":
+    send_msg("ITS FINE GET OUT THERE YOU WHIPPERSNAPPER")
 	
 	
 
